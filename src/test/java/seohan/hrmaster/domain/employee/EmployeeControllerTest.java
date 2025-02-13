566bb37e02b286a6008aa595ea74fa24b32e28fd
de123456sdf <cdaeun95@gmail.com>
2025-02-13 15:48:38 +0900

    feature: 사원 목록 조회 통합 테스트 코드 구현

--- src/test/java/seohan/hrmaster/domain/employee/EmployeeControllerTest.java
+++ src/test/java/seohan/hrmaster/domain/employee/EmployeeControllerTest.java
@@ -1,16 +1,19 @@
 package seohan.hrmaster.domain.employee;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import seohan.hrmaster.domain.employee.dto.request.EmployeeRequestDTO;
 
 import java.time.LocalDate;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,6 +31,7 @@ class EmployeeControllerTest {
 
     @Test
     @WithMockUser(username = "testUser", roles = {"USER"})
+    @DisplayName("사원 등록 API - 성공")
     void employeeCreate() throws Exception {
         // Given
         EmployeeRequestDTO requestDTO = new EmployeeRequestDTO(
@@ -49,5 +53,25 @@ class EmployeeControllerTest {
                 .andExpect(status().isCreated()) // 201 Created 확인
                 .andExpect(jsonPath("$.message").value("사원 등록 성공"))
                 .andDo(print());
+    }
+
+    @Test
+    @WithMockUser(username = "testUser", roles = {"USER"})
+    @DisplayName("사원 목록 조회 API - 성공")
+    void getAllEmployee_Success() throws Exception {
+        mockMvc.perform(get("/api/employees")
+                        .param("page", "1")  // 첫 번째 페이지 요청
+                        .param("size", "10") // 10개씩 조회
+                        .param("sort", "createdAt,desc") // 최신순 정렬
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()) // HTTP 200 응답 확인
+                .andExpect(jsonPath("$.message").value("사원 목록 조회 성공")) // 응답 메시지 검증
+                .andExpect(jsonPath("$.data.currentPage").value(1)) // 페이지 번호 검증
+                .andExpect(jsonPath("$.data.totalPages").isNumber()) // 전체 페이지 수 확인
+                .andExpect(jsonPath("$.data.totalElements").isNumber()) // 전체 데이터 개수 확인
+                .andExpect(jsonPath("$.data.pageSize").value(10)) // 한 페이지 크기 검증
+                .andExpect(jsonPath("$.data.responseDTOList").isArray()) // employees가 배열인지 확인
+                .andExpect(jsonPath("$.data.responseDTOList[0].name").isString()) // 첫 번째 직원의 이름이 존재하는지 확인
+                .andExpect(jsonPath("$.data.responseDTOList[0].email").isString()); // 첫 번째 직원의 이메일이 존재하는지 확인
     }
 }
